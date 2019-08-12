c2af5cd9621611b4cd442da748c6677d2679d551
ctj <3264275716@qq.com>
2019-08-12 16:43:47 +0800

    1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const cors=require("cors")
+const session=require('express-session');
+var app=express();
+
+//配置跨域
+app.use(cors({
+    origin:"*",//允许所有的跨域资源访问
+    credentials:true,//请求验证
+}))
+
+//配置session模块
+app.use(session({
+    secret:'128位安全字符串',
+    resave:true,//请求时更新数据
+    saveUninitialized:true,//保存初始数据
+}))
+
+//配置项目静态目录
+app.use(express.static("public"));
+
+app.listen(8080,()=>{
+   console.log("创建8080成功") 
+});
+
