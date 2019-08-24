e284e5e98761d7ac5f11d53c5fe65b01fc30f351
ctj <3264275716@qq.com>
2019-08-24 17:40:56 +0800

    11

--- router/menutype.js
+++ router/menutype.js
@@ -3,10 +3,12 @@ const pool = require('../pool.js');
 var router = express.Router();
 
 router.get("/menutype", (req, res) => {
-    var type = req.query.type;
+	var type = req.query.type;
+	var start=parseInt(req.query.start);
+	var count=parseInt(req.query.count);
 	//console.log(type);
-    var sql = `select * from menu where type=?`
-    pool.query(sql,[type],(err, result) => {
+    var sql = `select * from menu where type=? LIMIT ?,?`
+    pool.query(sql,[type,start,count],(err, result) => {
         if(err)throw err;
 	   if(result.length>0){
 		   res.send({code:1,data:result});
