72d9e8fd81d5804f50c508fe06bba8b47ebf3c0d
ctj <3264275716@qq.com>
2019-08-24 16:10:56 +0800

    2.1

--- router/menu.js
+++ router/menu.js
@@ -4,11 +4,39 @@ var router = express.Router();
 /*根据菜单id查询*/
 router.get("/menu", (req, res) => {
     var mid = req.query.mid;
-    var sql = `SELECT * FROM menu WHERE mid=?`
-    pool.query(sql, [mid], (err, result) => {
+    console.log(mid);
+    var data = {};
+    var sqla = `SELECT * FROM menu WHERE mid=?`
+    pool.query(sqla, [mid], (err, result) => {
         if (err) throw err;
-        res.send(result);
+        //console.log(result);
+        data.menu = result[0]; 
+        var sqlb = `SELECT * FROM materials WHERE mid=?`
+        pool.query(sqlb, [mid], (err, result) => {
+            if (err) throw err;
+            data.materials = result;
+            var sqlc = `SELECT * FROM step WHERE mid=?`
+            pool.query(sqlc, [mid], (err, result) => {
+                if (err) throw err;
+                data.step = result;
+                if (result.length > 0) {
+                    console.log(data)
+                    res.send(data);
+                } else {
+                    res.send({ code: 1, msg: "shibai" });
+                }
+        
+                // res.send(result);
+            })
+            // res.send(result);
+        })
+
+
+
+        
     })
+
+
 })
 
 module.exports = router;
